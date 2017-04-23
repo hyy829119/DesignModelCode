fa29ca19e663ecb4eb0a9322fffcb3be31db8842
hyy <hyy829119@163.com>
2017-04-23 13:45:05 +0800

    12
    设计模式

---
+++ DesignPatterns/designpattrns-SingleModel/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static designpattrns_SingleModel.cto;
+
+namespace designpattrns_SingleModel
+{
+
+    /*单例模式：用大白话的理解，程序员与CTO的关系,一个公司的CTO只有一个,每个程序员每天都面对的是同一个CTO
+               在代码的世界里,CTO是cto类的一个实例，每个程序员调用cto类获取CTO对象的时候，肯定要求是同一个
+               人,这是公司组织架构决定的,因此我们需要保证每个程序员来向CTO汇报工作的时候(调用CTO类),都是同一个
+               CTO对象,所以cto类需要保证的是每次返回的实例（CTO）必须一样，不然,就容易产生多个CTO,既违背组织原则,
+               也容易导致管理混乱,关键是一个公司也不能存在多个CTO....*/
+
+    /// <summary>
+    /// 该类相当于老板
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///现在老板说开始工作,不要扯写没用的 
+            ///干活的都是程序员,因此通知程序员吧,公司程序员就3个人.....
+            for (int i = 0; i < 3; i++)
+            {
+                cto leader = cto.getInstance();  //领导出场
+                leader.Say();                    //讲话了
+                programer pg = new programer(leader, i.ToString());//程序员心中应该随时把CTO的话记在心里,我时刻就是把我们CTO的话记在心里的.....
+                pg.report();
+            }
+
+            //此代码是用来防止程序退出的(Net程序员您懂的)
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// cto类负责产生CTO
+    /// </summary>
+
+    public class cto
+    {
+        //private static cto CTO = new cto(); //生产一个CTO
+
+        ////不让用户程序员自己随便new对象,否则容易产生多个CTO 
+        //private cto()
+        //{
+
+        //}
+
+        ///// <summary>
+        ///// 单例模式的核心
+        ///// </summary>
+        ///// <returns></returns>
+        //public static cto getInstance() { return CTO; }
+
+
+        //public void Say()
+        //{
+        //    /*程序的屌丝内心,现实中不是CTO,只能在程序世界里面自我封官了哈。。。。*/
+        //    Console.WriteLine("我是公司的CTO,你们的工作需要想问汇报!");
+        //}
+
+
+        private static cto CTO = null; //生产一个CTO
+
+        //不让用户程序员自己随便new对象,否则容易产生多个CTO 
+        private cto()
+        {
+
+        }
+
+        /// <summary>
+        /// 单例模式的核心
+        /// </summary>
+        /// <returns></returns>
+        public static cto getInstance()
+        {
+            if (CTO == null) CTO = new cto();
+            return CTO;
+        }
+
+
+        public void Say()
+        {
+            /*程序的屌丝内心,现实中不是CTO,只能在程序世界里面自我封官了哈。。。。*/
+            Console.WriteLine("我是公司的CTO,你们的工作需要想问汇报!");
+        }
+    }
+
+    /// <summary>
+    /// 程序员
+    /// </summary>
+    public class programer
+    {
+        /// <summary>
+        /// 把cto通过构造的方式传入,相当于程序员也时刻知道自己归谁领导... pmp也很重要......
+        /// </summary>
+        private cto ctoLeader;
+        private string Name;
+        public programer(cto CTOLeader, string name)
+        {
+            this.ctoLeader = CTOLeader;
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// 开始汇报
+        /// </summary>
+        public void report()
+        {
+            Console.WriteLine($"我是公司的Java程序员-{Name},请你检阅我的工作!");
+        }
+    }
+
+}
---
+++ DesignPatterns/designpattrns-SingleModelEx/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace designpattrns_SingleModelEx
+{
+
+    /// <summary>
+    /// cto类负责产生CTO
+    /// </summary>
+
+    public class cto
+    {
+        private static int maxnumofcto = 5; //申明返回的实例数量
+
+        private static List<string> ctolistprop = new List<string>(); //每个cto都有自己的属性,因此用来存储每个对象私有属性
+
+        private static List<cto> ctolist  //默认申请指定数量的CTO对象存储
+        {
+            get
+            {
+                List<cto> ctolisttemp = new List<cto>();
+                for (int i = 0; i < maxnumofcto; i++)
+                {
+                    ctolisttemp.Add(new cto($"我是CTO,我的名字是{i}"));
+                };
+                return ctolisttemp;
+            }
+        }
+
+        private static int countnumofcto = 0;//申明cto的的序列号,便于后边区分
+
+        //不让用户程序员自己随便new对象,否则容易产生多个CTO 
+        //同时把生产指定数量的对象放入数组中
+        private cto()
+        {
+
+        }
+
+        /// <summary>
+        /// 用来存储CTO的姓名
+        /// </summary>
+        /// <param name="name"></param>
+        private cto(string name)
+        {
+            ctolistprop.Add(name);
+        }
+
+
+        /// <summary>
+        /// 单例模式的核心
+        /// </summary>
+        /// <returns></returns>
+        public static cto getInstance()
+        {
+            /*下面的核心从cto队列中随机返回一个CTO对象*/
+            Random random = new Random();
+            countnumofcto = random.Next(maxnumofcto);
+            return ctolist[countnumofcto];
+        }
+
+
+        public void Say()
+        {
+            Console.WriteLine($"我是公司的CTO{ctolistprop[countnumofcto]},你们的工作需要想问汇报!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                cto CTO = cto.getInstance(); //获取cto实例,并且每次返回都随机的
+                Console.WriteLine($"第{j}次调用的对象为:");
+                CTO.Say();
+                Thread.Sleep(1000);//暂停目的,不暂停的话,在返回实例的时候有可能每次返回的都是同一个实例，因为时间间隔小的随机数生产可能相同
+            }
+        }
+    }
+}
